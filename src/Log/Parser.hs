2562e9cd1132e1012ce684da5c32c0938ed8fe0f
Marc Juchli <mail@marcjuch.li>
2015-03-31 12:45:51 +0200

    rename function so name log is more consistent

--- src/Log/Parser.hs
+++ src/Log/Parser.hs
@@ -7,17 +7,17 @@ import Log.Types
 import Data.Binary.Get
 import qualified Data.ByteString.Lazy as BL
 
-getMessageSets :: Get Log
-getMessageSets = do
+getLog :: Get Log
+getLog = do
   empty <- isEmpty
   if empty
       then return []
       else do messageSet <- messageSetParser
-              messageSets <- getMessageSets
+              messageSets <- getLog
               return (messageSet:messageSets)
 
 parseLog :: String -> IO Log
 parseLog a = do
   input <- BL.readFile a
-  return (runGet getMessageSets input)
+  return (runGet getLog input)
 
