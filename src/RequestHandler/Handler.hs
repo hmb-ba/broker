008423a70c023f0355e6fe5f9c17a4c44b7bf500
Marc Juchli <mail@marcjuch.li>
2015-04-01 14:07:25 +0200

    remove comments

--- src/RequestHandler/Handler.hs
+++ src/RequestHandler/Handler.hs
@@ -12,28 +12,25 @@ import Control.Concurrent
 import Network.Parser
 
 initHandler :: IO Socket
-initHandler = do 
-  sock <- socket AF_INET Stream 0 
-  setSocketOption sock ReuseAddr 1 
+initHandler = do
+  sock <- socket AF_INET Stream 0
+  setSocketOption sock ReuseAddr 1
   bindSocket sock (SockAddrInet 4343 iNADDR_ANY)
-  listen sock 2 
+  listen sock 2
   return sock
 
 listenLoop :: Socket -> IO()
-listenLoop sock =  do 
-  print "A"
-  conn <- accept sock 
-  print "Aa"
+listenLoop sock =  do
+  conn <- accept sock
   readStream conn
-  listenLoop sock 
+  listenLoop sock
 
-readStream :: (Socket, SockAddr) -> IO() 
-readStream (sock, sockaddr) = do 
-  print "B"
-  input <- SockBL.recv sock 4096 
+readStream :: (Socket, SockAddr) -> IO()
+readStream (sock, sockaddr) = do
+  input <- SockBL.recv sock 4096
   let request = input
   requestMessage <- readRequest request
-  print requestMessage 
-  return () 
+  print requestMessage
+  return ()
 
 
