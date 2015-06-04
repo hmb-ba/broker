bce72bfee2fe42ef95db3d0be4e00f2a771a15d7
SherwoodCH <lorenz.wolf@bluewin.ch>
2015-06-04 11:44:42 +0200

    restructure benchmark producer

--- benchmark/producer/Main.hs
+++ benchmark/producer/Main.hs
@@ -31,29 +31,31 @@ main = do
   -----------------
   sock <- socket AF_INET Stream defaultProtocol
   setSocketOption sock ReuseAddr 1
-  let ip = toHostAddress (read "127.0.0.1" :: IPv4)
-  --let ip = toHostAddress (read "152.96.195.205" :: IPv4)
+  --let ip = toHostAddress (read "127.0.0.1" :: IPv4)
+  let ip = toHostAddress (read "152.96.193.212" :: IPv4)
   --let ip = toHostAddress (read "152.96.195.4" :: IPv4)
   connect sock (SockAddrInet 4343 ip)
 
-  -------------------------
-  -- Send / Receive Loop
-  -------------------------
   -- FIXME (meiersi): I'd also recommend making the length of this string a
   -- command-line parameter to simplify tests.
   -- FIXME (meiersi): also consider whether you can create criterion
   -- microbenchmarks for all relevant parts of the message processing code
   -- path. This will help you pinpoint performance problems and direct your
   -- optimization efforts.
-  randBytes <- getEntropy 100
+
+
+  let numberOfBytes = 1000
+  let batchSize = 10
+
+  randBytes <- getEntropy numberOfBytes
 
   let topicA = stringToTopic "performance-0"
   let topicB = stringToTopic "performance-1"
   let clientId = stringToClientId "benchmark-producer"
-  let bytes = [randBytes | x <- [1..10]]
+  let bytes = [randBytes | x <- [1..batchSize]]
 
   let head = Head 0 0 clientId
-  let req = Produce head [ ToTopic topicA [ ToPart 0 bytes, ToPart 1 bytes], ToTopic topicB [ToPart 0 bytes] ]
+  let req = Produce head [ ToTopic topicA [ ToPart 0 bytes]]
 
   replicateM_ 1000000 (sendRequest sock $ req)
   putStrLn "done produce"
